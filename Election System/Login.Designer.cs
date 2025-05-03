e65932bf8c88def3dc4ec6c85b66d993240fbef9
Glyzel <Administrator@PC02>
2025-05-03 12:52:39 +0800

    gfh

--- Election System/Login.Designer.cs
+++ Election System/Login.Designer.cs
@@ -28,20 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.voters_id_login = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_register = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // voters_id_login
+            // 
+            this.voters_id_login.Location = new System.Drawing.Point(38, 115);
+            this.voters_id_login.Name = "voters_id_login";
+            this.voters_id_login.Size = new System.Drawing.Size(127, 20);
+            this.voters_id_login.TabIndex = 1;
+            this.voters_id_login.TextChanged += new System.EventHandler(this.voters_id_login_TextChanged);
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(38, 141);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Welcome back, Students!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Don\'t have an account yet?";
+            // 
+            // btn_register
+            // 
+            this.btn_register.Location = new System.Drawing.Point(183, 181);
+            this.btn_register.Name = "btn_register";
+            this.btn_register.Size = new System.Drawing.Size(75, 23);
+            this.btn_register.TabIndex = 5;
+            this.btn_register.Text = "Register";
+            this.btn_register.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(477, 469);
+            this.ClientSize = new System.Drawing.Size(454, 467);
+            this.Controls.Add(this.btn_register);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.voters_id_login);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox voters_id_login;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_register;
     }
 }
 
--- Election System/Login.cs
+++ Election System/Login.cs
@@ -16,5 +16,41 @@ namespace Election_System
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = voters_id_login.Text;
+
+            foreach (char c in id)
+            {
+                if (!char.IsDigit(c))
+                {
+                    MessageBox.Show("Please enter a valid ID");
+                    voters_id_login.Text = "";
+                    return;
+                }
+            }
+            if (id.Length == 8)
+            {
+                this.Dispose();
+                DashboardVoter dashboardVoter = new DashboardVoter();
+                dashboardVoter.Show();
+            }
+            else if (id.Length == 0)
+            {
+                MessageBox.Show("Please enter your ID");
+                voters_id_login.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Please enter your ID");
+                voters_id_login.Text = "";
+            }
+
+        }
+        private void voters_id_login_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
     }
 }
